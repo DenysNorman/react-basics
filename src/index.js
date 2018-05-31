0404205df0cc4815e0985117cc22f9df65ce8b4c
denysnorman <denys.norman.tr@gmail.com>
2018-05-31 17:31:37 +0300

    end of first ugly project

--- src/index.js
+++ src/index.js
@@ -8,13 +8,26 @@ import NewsList from './components/news_list'
 
 class App extends Component {
     state = {
-        news: JSON
+        news: JSON,
+        filtered: []
+    }
+
+    getKeyword = (event) => {
+        let keyword = event.target.value;
+        let filtered = this.state.news.filter((item) =>{
+             return item.title.indexOf(keyword) > -1
+        })
+        this.setState({
+            filtered
+        })
     }
     render(){
+        let newsFiltered = this.state.filtered;
+        let newsWhole = this.state.news;
         return(
             <div>
-                <Header/>
-                <NewsList news={this.state.news}>
+                <Header keywords={this.getKeyword} />
+                <NewsList news={this.state.filtered.length === 0 ? newsWhole : newsFiltered}>
                     <h3>News are:</h3>
                 </NewsList>
             </div>
